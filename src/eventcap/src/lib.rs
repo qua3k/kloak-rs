8cb0ee09e5c98f9046cf9ba11c8c86ede71e6f53
qua3k <cliffmaceyak@gmail.com>
2021-09-16 18:03:56 -0400

    Replace unwrap with defined pattern matching

--- src/eventcap/src/lib.rs
+++ src/eventcap/src/lib.rs
@@ -2,9 +2,16 @@ use evdev::Device;
 
 pub fn print_input(mut device: &Device) {
     loop {
-        // TODO: Get rid of unwrap
-        for ev in device.fetch_events().unwrap() {
-            println!("Type: {:?}    Code: {}    Value: {}", ev.event_type(), ev.code(), ev.value())
+        match device.fetch_events() {
+            Ok(iterator) => {
+                for ev in iterator {
+                    println!("Type: {:?}    Code: {}    Value: {}", ev.event_type(), ev.code(), ev.value())
+                }
+            }
+            Err(e) => {
+                eprintln!("fetching events failed: {}", e);
+                process::exit(1)
+            }
         }
     }
 }
