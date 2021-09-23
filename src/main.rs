91df4bf1cb1a5a37562fd197c4bda135412a1187
qua3k <cliffmaceyak@gmail.com>
2021-09-23 18:37:09 -0400

    Implement device grabbing
    We are waiting for this to land in the evdev crate

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,22 @@ mod args;
 fn main() {
     let (input, max_delay, verbose) = get_args();
 
-    let device = match Device::open(input) {
+    let mut device = match Device::open(input) {
         Ok(d) => d,
         Err(e) => {
             eprintln!("could not open device: {}", e);
             process::exit(1)
         }
     };
+    
+    // Grab the device to receive all events
+    // This has not landed in stable yet, so we cannot use it
+    /*
+    if let Err(e) = device.grab() {
+        eprintln!("grabbing device failed: {}", e);
+        process::exit(1)
+    } 
+    */
 
     loop {
         if let Err(e) = emit_delay(&device, max_delay, verbose) {
